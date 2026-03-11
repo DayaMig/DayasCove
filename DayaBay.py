7fa31b68a1a11eca0abd47ef4ead251df1835270
daya <aldayamigrussel@gmail.com>
2026-03-11 16:31:20 +0800

    Replace old code with clicker game

--- DayaBay.py
+++ DayaBay.py
@@ -1,8 +1,25 @@
 import streamlit as st
+import time
 
-st.title("Mig Russel Aldaya")
-st.write("Welcome here gang")
+st.title("Click as fast as you can!")
+st.write("Click that button as fast as you possibly can gang")
 
-name = st.text_input("Enter yo name")
-if name:
-    st.write("Hello,", name)
+if 'score' not in st.session_state:
+    st.session_state.score = 0
+    TIME_LIMIT = 10
+    elapsed_time = time.time() - st.session_start_time
+
+    if not st.session_state.game_over:
+        st.write(f"time left:  {max(0,int(TIME_LIMIT - elapsed_time))}s")
+
+        if st.button("CLICK ME"):
+            st.session_state.score += 1
+
+            if elapsed_time >= TIME_LIMIT:
+                st.session_state.game_over = True
+                st.success(f"Time's up. Your Final Score is: {st.session_state.score}")
+
+                if st.button("Play Again"):
+                    st.session_state.score = 0
+                    st.session_state.start_time - time.time()
+                    st.session_state.game_over = False
